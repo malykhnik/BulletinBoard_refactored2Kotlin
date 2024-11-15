15b543cc59e52a2395fd41a7edd1837f09b79bf3
Nikita <malykhnik@mail.ru>
2024-11-15 22:45:21 +0400

    some fix in controller

--- src/main/kotlin/com/malykhnik/bulletinboard_kotlin/configuration/SecurityConfiguration.kt
+++ src/main/kotlin/com/malykhnik/bulletinboard_kotlin/configuration/SecurityConfiguration.kt
@@ -2,21 +2,50 @@ package com.malykhnik.bulletinboard_kotlin.configuration
 
 import com.malykhnik.bulletinboard_kotlin.configuration.properties.JwtProperties
 import com.malykhnik.bulletinboard_kotlin.repository.UserRepository
+import com.malykhnik.bulletinboard_kotlin.service.security_logic.JwtAuthFilter
 import com.malykhnik.bulletinboard_kotlin.service.security_logic.MyUserDetailsService
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableConfigurationProperties(JwtProperties::class)
-class SecurityConfiguration {
+class SecurityConfiguration(
+    private val authProvider: AuthenticationProvider
+) {
+    @Bean
+    fun securityFilterChain(
+        http: HttpSecurity,
+        jwtAuthenticationFilter: JwtAuthFilter
+    ): DefaultSecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .authorizeHttpRequests {
+                it
+                    .requestMatchers("/api/auth", "api/auth/refresh", "/error").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/api/user").permitAll()
+                    .requestMatchers("/api/user**").hasRole("ADMIN")
+                    .anyRequest().fullyAuthenticated()
+            }
+            .sessionManagement {
+                it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }
+            .authenticationProvider(authProvider)
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+        return http.build()
+    }
     @Bean
     fun userDetailsService(userRepository: UserRepository): UserDetailsService =
         MyUserDetailsService(userRepository)
